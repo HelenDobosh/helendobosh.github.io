f9ffb18784379c0c03ae874594ead976fbde0dca
odobosh <o.o.dobosh@gmail.com>
2021-06-22 16:36:39 +0300

    Moved script to separate file

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => input.addEventListener('input', updateValue))
+
+function updateValue(event) {
+    const parent = event.target.parentElement;
+    parent.classList.add('filled');
+    if(event.target.value === '') {
+        parent.classList.remove('filled');
+    }
+}
